a14764b7affb1b272bb933911ec652a973ce68bf
Kirk Patrick <kirkgo@gmail.com>
2023-04-26 16:42:23 +0100

    Created starting point of the application.

---
+++ src/tech/kirk/hotelreservation/HotelApplication.java
@@ -0,0 +1,9 @@
+package tech.kirk.hotelreservation;
+
+import static tech.kirk.hotelreservation.MainMenu.mainMenu;
+
+public class HotelApplication {
+    public static void main(String[] args) {
+        mainMenu();
+    }
+}
